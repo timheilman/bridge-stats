63e558d33b5ea99b32cddaedfa93f4f518ae0314
Tim Heilman <tim@dv8.org>
2017-02-12 16:32:16 -0800

    More changes to the API the client would like to have.

--- lib/bridge_stats/builder.rb
+++ lib/bridge_stats/builder.rb
@@ -3,12 +3,16 @@ require 'portable_bridge_notation'
 
 class BridgeStats::Builder
   def initialize
-    pp Bridge::Card.all[0]
-    importer = PortableBridgeNotation::Importer.new
+    importer = PortableBridgeNotation::ImporterFactory.get_instance # get_best_effort might log instead of raise...
     importer.attach(self)
     importer.import # io; todo: get a test file in here
+    # todo: provide some error configuration; some way to error-out per-game
+
   end
 
+  def with_new_game(index_within_io: index_within_io)
+
+  end
   def with_dealt_card(direction: direction, rank: rank, suit: suit)
     pp Bridge::Card.for(suits: [suit], ranks: [rank])[0]
   end
