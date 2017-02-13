8e317559b954e0bbc5244a7d880d7fe579fb71a8
Tim Heilman <tim@dv8.org>
2017-02-12 16:15:38 -0800

    Write the API we would like to have; make dependency provide it later. Test intentionally broken.

--- lib/bridge_stats/builder.rb
+++ lib/bridge_stats/builder.rb
@@ -4,6 +4,12 @@ require 'portable_bridge_notation'
 class BridgeStats::Builder
   def initialize
     pp Bridge::Card.all[0]
-    pp PortableBridgeNotation::IoParser.each_game_string nil
+    importer = PortableBridgeNotation::Importer.new
+    importer.attach(self)
+    importer.import # io; todo: get a test file in here
+  end
+
+  def with_dealt_card(direction: direction, rank: rank, suit: suit)
+    pp Bridge::Card.for(suits: [suit], ranks: [rank])[0]
   end
 end
